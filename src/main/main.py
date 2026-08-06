0d91c1420ac974ee54691dd1e9552f63b9690ddc
Kpham02 <Kpham02@github.com>
2026-08-06 21:30:29 +0000

    Test run: 0/5 passed, 5 failed, 0 skipped - 2026-08-06T21:30:29.183Z

--- src/main/main.py
+++ src/main/main.py
@@ -63,7 +63,7 @@ def load_and_clean_users(file_path):
 
             cursor.execute(
                 "INSERT INTO users(firstName, lastName) VALUES (?,?,?)",
-                (int(row[0], row[1]))
+                ((row[0], row[1]))
             )
 
 
