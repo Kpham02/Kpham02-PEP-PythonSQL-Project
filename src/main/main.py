0c1763e5a055dad90bee903d433364dc29bb8d0c
Kpham02 <Kpham02@github.com>
2026-08-06 22:30:44 +0000

    Test run: 2/4 passed, 2 failed, 0 skipped - 2026-08-06T22:30:44.258Z

--- src/main/main.py
+++ src/main/main.py
@@ -56,12 +56,13 @@ def load_and_clean_users(file_path):
 
     with open(file_path, "r", newline = "") as file:
         reader = csv.reader(file)
+        
         next(reader)
 
         for row in reader:
             if len(row) != 2:
                 continue
-            if " " in row:
+            if "  " in row:
                 continue
 
             cursor.execute(
